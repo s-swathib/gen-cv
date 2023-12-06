f506624d78c41556f40bd52913c2efe7027a867b
aswaryhcl <aswary.tiwari@hcl.com>
2023-12-06 14:34:43 +0000

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -132,7 +132,7 @@ async function generateText(prompt) {
   console.log(`Input Message: ${JSON.stringify(messages)}`);
   await fetch(`/api/message`, { method: 'POST', headers: { 'Content-Type': 'application/json'}, body: JSON.stringify(messages) })
   .then(response => response.text()) 
-  .then(console.log(`output Message: ${response.text()}`));
+  console.log(`output Message: ${response}`);
 
   addToConversationHistory(generatedText, 'light');
   if(products.length > 0) {
