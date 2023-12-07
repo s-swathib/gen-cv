3d12f9d891517667dceb6a8c9d019ef1719b99ae
aswaryhcl <aswary.tiwari@hcl.com>
2023-12-07 12:56:30 +0000

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -131,8 +131,12 @@ async function generateText(prompt) {
   let products
   console.log(`Input Message: ${JSON.stringify(messages)}`);
   await fetch(`/api/message`, { method: 'POST', headers: { 'Content-Type': 'application/json'}, body: JSON.stringify(messages) })
-  .then(response => response.text())
-  .then(data => console.log(data));
+  .then(response => response.json())
+  .then(data => {
+    generatedText = data["messages"][data["messages"].length - 1].content;
+    messages = data["messages"];
+    products = data["products"]
+  });
 
   //.then(console.log);
 
